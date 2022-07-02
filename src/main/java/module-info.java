36a2dbe173b46646cc536a25104f83d36a5c7108
tugalsan karabacak <tugalsan@gmail.com>
2022-07-03 00:00:11 +0300

    bug fixes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,10 @@ module com.tugalsan.api.input {
     requires java.desktop;
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.executable;
+    requires com.tugalsan.api.compiler;
     requires com.tugalsan.api.shape;
+    requires com.tugalsan.api.pack;
+    requires com.tugalsan.api.coronator;
     requires com.tugalsan.api.thread;
     requires com.tugalsan.api.ide.netbeans;
     requires com.tugalsan.api.file.sound;
