e53870daa309f152ef8eb9956a15c4c47364bc17
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:08:53 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.tugalsan.api.input {
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.union;
     
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     requires com.tugalsan.api.shape;
     requires com.tugalsan.api.tuple;
     
